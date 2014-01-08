277628a1d652882bc40d3ba75daf675224ff054b
Gabriela <gabicavalcantesilva@gmail.com>
2014-01-08 11:43:37 -0300

    reinitialized files

---
+++ save.php
@@ -0,0 +1,22 @@
+<?php
+	//open the database
+	$db = sqlite_open('sqlite:feedback.db');
+
+	if (!$db) die ($error);
+
+	$name = $_POST["name"]; 
+	$email = $_POST["email"]; 
+	$instensino = $_POST["instensino"]; 
+	$programa = $_POST["programa"];
+	$linguagens = $_POST["linguagens"];
+
+	//Insert record  
+	$conn = "INSERT INTO ladiesinfo (name, email, instensino, programa, linguagens) VALUES ('$name', '$email', '$instensino' , '$programa', '$linguagens');"
+	$ok = sqlite_exec($db, $conn);
+	if (!$ok) die() ("Algo de errado aconteceu");
+	//$db->exec("INSERT INTO ladiesinfo (name, email, instensino, programa, linguagens) VALUES ('&name', '&email', '&instensino' , '&programa', '&linguagens');");
+
+	echo "Seu cadastro foi realizado com sucesso!";
+	sqlite_close($db);
+	$db = NULL;
+?>
